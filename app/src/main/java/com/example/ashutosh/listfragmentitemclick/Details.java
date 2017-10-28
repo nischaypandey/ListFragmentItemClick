492af665fa467b93e1a5cf81881eeaaf09b58242
Vibhor <vibhor1608@gmail.com>
2017-10-28 22:19:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashutosh/listfragmentitemclick/Details.java
@@ -0,0 +1,18 @@
+package com.example.ashutosh.listfragmentitemclick;
+
+/**
+ * Created by Ashutosh on 17-06-2017.
+ */
+
+//Class to store the data of students.
+public class Details {
+
+  //String array of titles.
+    static String titleName[] = new String[] {
+            "Layout 1",
+            "Layout 2",
+            "Layout 3",
+            "Layout 4"
+    };
+
+}
